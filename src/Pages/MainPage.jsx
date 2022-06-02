1670f99a8a2f977830c8884df966305dbe8eb0bf
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-06-02 15:18:12 +0530

    mainPage

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AsideBar from '../Componenets/AsideBar'
 import Navbar from '../Componenets/Navbar'
+import LandingPage from './LandingPage'
 
 function MainPage() {
     return (
       <>
       <Navbar />
-            <AsideBar />
+        <AsideBar />
+        <LandingPage/>
         </>
   )
 }
