f54959c0fb33bd7dadf3421805b160c361f941d3
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-06-02 14:38:03 +0530

    first-comit-landing-page

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import image from '../asset/undraw_Ideas_flow_re_bmea.png';
+// import {Button} from '@mui/material'
+function LandingPage() {
+    return (
+        <>
+            <div className='landing-page-container'>
+    <div className='landing-wrapper' >
+        <div className='landingpage-header'>
+        MY Note App
+        </div>
+        <div className='heading-2'>Meet your Persnal</div>
+        <div className='heading-2-color'>Note taking App</div>
+        <p className='pera-1' >Manage your daily tasks and workflow in a modern way and boost your efficiency without any efforts.</p>
+       {/* <Button sx={{width:"10rem", margin:"8rem 0 0"}} variant='contained' >join now</Button> */}
+        <a className='have-account' href='#'>Already have a Account</a>
+    </div>
+    <div>
+                    <img className='landing-page-image'                src={image} />
+                </div>
+                </div>
+        </>
+)
+}
+
+export default LandingPage
