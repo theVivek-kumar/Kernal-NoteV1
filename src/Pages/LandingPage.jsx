ca0e3e174bb0978e281a6cd64db6b86acbcad4cf
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-06-02 15:33:13 +0530

    mainpage

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import image from '../asset/undraw_Ideas_flow_re_bmea.png';
-// import {Button} from '@mui/material'
+import image from '../asset/undraw_Ideas_flow_re_bmea (1).png';
+import {Button} from '@mui/material'
 function LandingPage() {
     return (
         <>
@@ -12,7 +12,7 @@ function LandingPage() {
         <div className='heading-2'>Meet your Persnal</div>
         <div className='heading-2-color'>Note taking App</div>
         <p className='pera-1' >Manage your daily tasks and workflow in a modern way and boost your efficiency without any efforts.</p>
-       {/* <Button sx={{width:"10rem", margin:"8rem 0 0"}} variant='contained' >join now</Button> */}
+       <Button sx={{width:"10rem", margin:"8rem 0 0"}} variant='contained' >join now</Button>
         <a className='have-account' href='#'>Already have a Account</a>
     </div>
     <div>
