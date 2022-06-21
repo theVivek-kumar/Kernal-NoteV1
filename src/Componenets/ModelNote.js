d6e35647326171037f53413977307dbb79037429
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-06-22 01:42:03 +0530

    model

--- src/Componenets/ModelNote.js
+++ src/Componenets/ModelNote.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
+
 
 function ModelNote() {
   return (
+    <>
       <div>
           <div className='model-container-area'>
               <div>Title</div>
-              <div></div>
+          <div><AiOutlineClose /></div>
+          <input type={text}/>
+          
 
               
           </div>
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,7 @@
 // import Search from '@mui/icons-material/Search'
 import React from 'react'
 import AsideBar from '../Componenets/AsideBar'
+import ModelNote from '../Componenets/ModelNote'
 import NavBar from '../Componenets/Navbar'
 import SearchAppBar from '../Componenets/Search'
 function MainPage() {
@@ -8,6 +9,7 @@ function MainPage() {
       <>
         <NavBar />
         <AsideBar />
+        <ModelNote/>
         </>
   )
 }
