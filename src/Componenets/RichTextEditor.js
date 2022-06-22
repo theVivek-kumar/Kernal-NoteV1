c0b02eafc016c6c068993c0e6d93bfbcd74a64f0
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-06-23 02:32:57 +0530

    feat

--- src/Componenets/RichTextEditor.js
+++ src/Componenets/RichTextEditor.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import ReactQuill from 'react-quill'; 
-//  import * as ReactQuill from 'react-quill';
+import React, {useState} from 'react';
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
-function RichTextEditor() {
-  return (
-    <div><ReactQuill/></div>
-  )
-}
+const RichTextEditor = () => {
 
-export default RichTextEditor
+    const [show, setShow] = useState('')
+
+    return (
+        <>
+            <div>
+                <ReactQuill className="shadow-sm"
+                            theme="snow"
+                            style={{
+                                height: 350,
+                                marginTop: '1rem',
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}
+
+                            value={show}
+
+                            modules={{
+                                toolbar: [
+                                    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }], [{size: []}],
+                                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                                    [{'align': []}],
+                                    [{ 'color': [] }, { 'background': [] }],
+                                    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+                                    ['link', "video","image", "code-block"],
+                                    ['clean']
+                                ],
+                            }}
+                            formats={[
+                                'header', 'font', 'size',
+                                'bold', 'italic', 'underline', 'strike', 'blockquote', 'color', 'background',
+                                'list', 'bullet', 'indent', 'link', 'video', 'image', "code-block", "align"
+                            ]}
+                            onChange={(val) => {
+                                setShow(val)
+                            }}
+                />
+          </div>
+        </>
+    );
+};
+
+export default RichTextEditor;
