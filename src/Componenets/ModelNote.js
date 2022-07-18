7cd2f7c4ced73fdaf3518e672f12d627fc59b025
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-07-18 17:39:50 +0530

    complete model

--- src/Componenets/ModelNote.js
+++ src/Componenets/ModelNote.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai';
-import {MdClose} from "react-icons/md";
+import {MdClose,MdOutlineColorLens} from "react-icons/md";
 import RichTextEditor from './RichTextEditor';
 import { useNoteContext } from '../Context/NoteContext';
 
@@ -53,28 +53,107 @@ function ModelNote() {
               }}
               className="close"></AiOutlineClose></div></div>
           
-          <input className='input-field-area' placeholder='add your note here' />
-          <div className='title-container'>lable</div>
-          <div className='ritcheditor-container'><RichTextEditor /></div>
+              
+                  <input 
+                  placeholder="enter title of note"
+                  value={noteState.title}
+                  required
+                  onChange={(e) =>
+                    noteDispatch({ type: "TITLE", payload: e.target.value })
+                  }
+                  className='input-field-area'
+                />
+          <div className='title-container'>Description</div>
+          <div className='ritcheditor-container'><RichTextEditor  textAreaValue={textareaValue}/></div>
           
           <div className='lable-flow-flex'>
-          <input type="radio" />
-          <p className="label">Office</p>
-          <input type="radio" />
-          <p className="label">Home</p>
-          <input type="radio" />
-          <p className="label">School</p>
+                    <input
+            type="radio"
+                      name="label"
+                      required
+                      className=""
+                      id="medium"
+                      checked={label === "office"}
+                      onChange={() =>
+                        noteDispatch({ type: "LABEL", payload: "office" })
+                      }          />
+                      <p className="label">Office</p>
+                    <input
+                      type="radio"
+                      name="label"
+                      required
+                      className=""
+                      id="low"
+                      checked={label === "home"}
+                      onChange={() =>
+                        noteDispatch({ type: "LABEL", payload: "home" })
+                      }       
+                    />
+                    <p className="label">Home</p>
+                    <input
+                      type="radio"
+                      name="label"
+                      required
+                      className=""
+                      id="high"
+                      checked={label === "school"}
+                      onChange={() =>
+                        noteDispatch({ type: "LABEL", payload: "school" })
+                      }
+                        />
+                    <p className="label">School</p>
+                    
           </div>
           <div className='title-container'>priority</div>
           <div className='lable-flow-flex'>
-          <input type="radio" />
-          <p className="label">High</p>
-          <input type="radio" />
-          <p className="label">Medium</p>
-          <input type="radio" />
+                    <input
+                    type="radio"
+                      name="priority"
+                      required
+                      value={3}
+                      className=""
+                      id="high"
+                      checked={priority === "urgent"}
+                      onChange={() =>
+                        noteDispatch({ type: "PRIORITY", payload: "urgent" })
+                      }/>
+                      <p className="label">High</p>
+                    <input
+                      type="radio"
+                      name="priority"
+                      required
+                      value={2}
+                      className=""
+                      id="medium"
+                      checked={priority === "medium"}
+                      onChange={() =>
+                        noteDispatch({ type: "PRIORITY", payload: "medium" })
+                      }          />
+                    <p className="label">Medium</p>
+                    <input
+                      type="radio"
+                      name="priority"
+                      required
+                      value={1}
+                      className=""
+                      id="low"
+                      checked={priority === "low"}
+                      onChange={() =>
+                        noteDispatch({ type: "PRIORITY", payload: "low" })
+                      }          />
             <p className="label">Low</p>
             </div>
-            <div className='model-btn-addnote'>
+                  <div className='model-btn-addnote'>
+                     <span className="color-picker-icon">
+                <MdOutlineColorLens />
+              </span>
+
+              <input
+                type="submit"
+                value={isEdit ? "Update" : "Add Note"}
+                style={{ position: "absolute", bottom: "1rem", right: "1rem" }}
+                className="submit-btn"
+              />
             <button className='btn-add'>ADD NOTE</button>
             </div>
           
