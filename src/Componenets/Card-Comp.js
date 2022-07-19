d08877143034fb386342cc897cad329d9a245847
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-07-19 12:09:53 +0530

    create card-context

--- src/Componenets/Card-Comp.js
+++ src/Componenets/Card-Comp.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import { BsPinAngle } from 'react-icons/bs';
 import { BiEdit,BiArchiveIn,BiTrashAlt} from 'react-icons/bi';
+import { useNoteContext } from '../Context/NoteContext';
+function CardComp({ note }) {
+      const {
+    _id,
+    title,
+    textareaValue,
+    label,
+    labelArray,
+    priority,
+    notesBgColor,
+    noteCreatedDate,
+    isEdit,
+  } = note;
+  let { noteDispatch, adddToArchive, addToNotes, adddToTrash, pinnedNotes, deleteFromTrash } = useNoteContext();
 
-function CardComp() {
+  console.log("pinned",pinnedNotes)
+  console.log("all", addToNotes)
+
+  console.log("pinnedNOtesafterdelete", pinnedNotes)
+  
+
+  function priorityColor(priority) {
+    switch (priority) {
+      case "urgent":
+        return "red";
+
+      case "medium":
+        return "orange";
+
+      case "low":
+        return "green";
+
+      default:
+        break;
+    }
+  }
 return (
     <>
-        <div className='main-card-containr'>
+        <div className='main-card-containr'
+        style={{ backgroundColor: notesBgColor }}>
             <div className='card-container'>
                 <div className='section-1'>
                     <div>
-                        Title
+                        {title}
                 </div>
                 <div><BsPinAngle/></div>
                 </div>
                 <div>lable</div>
                 <div>lable</div>
                 <div className='section-4'>
-                    <div>
-                    date
-                    </div>
+                     <p className="note-card-created">created on {noteCreatedDate}</p>
                     <div>time</div>
-                    <div><BiEdit /></div>
-                    <div><BiArchiveIn /></div>
-                    <div><BiTrashAlt/></div>
+                    <div><BiEdit
+                     onClick={() => {
+                noteDispatch({ type: "IS_EDIT", payload: true });
+                noteDispatch({ type: "NOTE_MODAL", payload: true });
+                noteDispatch({
+                  type: "EDIT_NOTE",
+                  payload: note,
+                });
+              }}/></div>
+                    <div><BiArchiveIn onClick={() => adddToArchive(note, noteDispatch)}/></div>
+                    <div><BiTrashAlt onClick={() => adddToTrash(note, noteDispatch)} /></div>
                 </div>
         
             </div>
